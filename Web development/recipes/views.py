a61ed34ff4d6d94d99aa282a450b8d6ca7b92a2a
Thiago Pereira <tlup.09@hotmail.com>
2025-07-15 16:48:15 -0300

    Iniciando o frontend

--- Web development/recipes/views.py
+++ Web development/recipes/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request=request, template_name='recipes/home.html')
+    return render(request=request, template_name='recipes/pages/home.html', context={
+                                                                    'owner': 'Thiago Pereira'
+                                                                                })
